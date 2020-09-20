64c66993fc86b01f92a9fb329cfe2c7235cedb82
alihatatagithub <50938568+alihatatagithub@users.noreply.github.com>
2020-09-20 14:33:30 -0700

    Add files via upload

---
+++ RealTimeSignalRDotNetCore/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTimeSignalRDotNetCore.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string fromUser, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage", fromUser, message);
+
+        }
+    }
+}
